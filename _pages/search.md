4fc9568141003347894558b5ae4dfeec3c842f0a
slowdive14 <xinlisupreme@naver.com>
2023-02-23 08:57:05 +0900

    s

---
+++ _pages/search.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: Search
+---
+
+<div id="search-container">
+  <input type="text" id="search-input" placeholder="Search...">
+  <ul id="results-container"></ul>
+</div>
+
+<script src="{{ '/assets/js/search.js' | relative_url }}"></script>
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,83 +1,56 @@
 ---
+layout: null
 ---
-(function() {
-  var searchTerm, searchButton, results, resultList, index;
 
-  function highlightResult(str, search) {
-    return str.replace(
-      new RegExp(search, "gi"),
-      (match) => `<mark>${match}</mark>`
-    );
+(function() {
+  var searchTerm, index, results;
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, ' '));
+      }
+    }
   }
 
-  function search(query) {
-    results = [];
+  function search() {
+    searchTerm = getQueryVariable('query');
+    if (!searchTerm) {
+      return;
+    }
     index = lunr(function() {
-      this.ref("id");
-      this.field("title", { boost: 10 });
-      this.field("description", { boost: 5 });
-      this.field("content");
+      this.field('title');
+      this.field('content');
+      this.ref('id');
+      {% assign count = 0 %}
       {% for post in site.posts %}
-      this.add({
-        id: {{ post.id }},
-        title: {{ post.title | jsonify }},
-        description: {{ post.description | jsonify }},
-        content: {{ post.content | strip_html | jsonify }}
-      });
+        {% assign count = count | plus: 1 %}
+        this.add({
+          title: {{ post.title | jsonify }},
+          content: {{ post.content | strip_html | jsonify }},
+          id: {{ count }}
+        });
       {% endfor %}
     });
+    results = index.search(searchTerm);
+    displayResults();
+  }
 
-    if (query !== "") {
-      searchTerm = query;
-      results = index.search(searchTerm).map(({ ref }) => parseInt(ref));
-      showResults();
+  function displayResults() {
+    var $results = $('#results-container');
+    if (results.length) {
+      $results.empty();
+      results.forEach(function(result) {
+        var item = '<li><a href="' + '{{ site.baseurl }}' + '/' + '{{ site.posts[result.ref].url }}' + '">' + '{{ site.posts[result.ref].title }}' + '</a></li>';
+        $results.append(item);
+      });
     } else {
-      hideResults();
+      $results.html('<li>No results found</li>');
     }
   }
 
-  function showResults() {
-    resultList.innerHTML = "";
-    results.forEach(function(result) {
-      var li = document.createElement("li");
-      var a = document.createElement("a");
-      var post = window.store[result];
-      a.setAttribute("href", post.url);
-      a.innerHTML = highlightResult(post.title, searchTerm);
-      li.appendChild(a);
-      resultList.appendChild(li);
-    });
-    resultsContainer.style.display = "block";
-  }
-
-  function hideResults() {
-    resultsContainer.style.display = "none";
-  }
-
-  function searchButtonClicked(event) {
-    event.preventDefault();
-    search(searchInput.value);
-  }
-
-  function searchInputInputed(event) {
-    event.preventDefault();
-    search(searchInput.value);
-  }
-
-  function closeResults(event) {
-    if (!event.target.closest("#search-results")) {
-      hideResults();
-    }
-  }
-
-  document.addEventListener("click", closeResults);
-
-  document.addEventListener("DOMContentLoaded", function() {
-    searchInput = document.querySelector("#search-form input[name='query']");
-    searchButton = document.querySelector("#search-form button");
-    resultsContainer = document.querySelector("#search-results");
-    resultList = document.querySelector("#search-results ul");
-    searchInput.addEventListener("input", searchInputInputed);
-    searchButton.addEventListener("click", searchButtonClicked);
-  });
+  search();
 })();
